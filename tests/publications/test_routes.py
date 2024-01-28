1170c54c9d55a3753355fed9ffe4d44ad860eece
redbird504 <timurza@icloud.com>
2024-01-28 13:41:27 +0300

    tests(publications): add test for get publications

--- tests/publications/test_routes.py
+++ tests/publications/test_routes.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pytest
 from fastapi.testclient import TestClient
 from fastapi import status
@@ -80,6 +82,28 @@ async def test_create_vote_auth(client: TestClient, db_session) -> None:
 
 
 @pytest.mark.asyncio
+async def test_update_vote_auth(client: TestClient, db_session) -> None:
+    vote = VoteFactory.create(grade=False)
+    user = VoteFactory.get_current_session().scalar(
+        select(User).where(User.id == vote.user_id)
+    )
+    credentials = UserFactory.get_credentials(user)
+
+    vote_in_db = await db_session.scalar(
+        select(Vote).where(Vote.publication_id == vote.publication_id, Vote.user_id == user.id)
+    )
+    assert vote_in_db.grade is False
+
+    resp = client.put(
+        f"/publications/{vote.publication_id}/vote",
+        json={"grade": True},
+        headers={"Authorization": credentials}
+    )
+    assert resp.status_code == status.HTTP_200_OK
+    assert resp.json()["details"]["grade"] is True
+
+
+@pytest.mark.asyncio
 async def test_remove_vote_auth(client: TestClient, db_session) -> None:
     vote = VoteFactory()
     user = VoteFactory.get_current_session().scalar(
@@ -108,3 +132,137 @@ def test_remove_vote_no_auth(client: TestClient) -> None:
     publication = PublicationFactory()
     resp = client.delete(f"/publications/{publication.id}/vote")
     assert resp.status_code == status.HTTP_403_FORBIDDEN
+
+
+def test_get_publications(client):
+    publication = PublicationFactory()
+    VoteFactory.create_batch(publication_id=publication.id, grade=True, size=3)
+    VoteFactory.create_batch(publication_id=publication.id, grade=False, size=2)
+
+    resp = client.get("/publications")
+    assert resp.status_code == status.HTTP_200_OK
+    publication_data = resp.json()["details"][0]
+    assert publication_data["rating"] == 1.0
+    assert publication_data["vote_count"] == 5
+
+
+def test_get_publications_ordering_by_rating_desc(client):
+    publication = PublicationFactory()  # rating 1
+    VoteFactory.create_batch(publication_id=publication.id, grade=True, size=3)
+    VoteFactory.create_batch(publication_id=publication.id, grade=False, size=2)
+
+    publication2 = PublicationFactory()  # rating -1
+    VoteFactory.create_batch(publication_id=publication2.id, grade=True, size=3)
+    VoteFactory.create_batch(publication_id=publication2.id, grade=False, size=4)
+
+    resp = client.get(
+        "/publications",
+        params={
+            "order_by": "rating",
+            "desc": True
+        }
+    )
+
+    true_order = [publication, publication2]
+    assert resp.status_code == status.HTTP_200_OK
+
+    publications_data = resp.json()["details"]
+    for i in range(2):
+        if publications_data[i]["id"] != true_order[i].id:
+            assert False
+
+
+def test_get_publications_ordering_by_rating_asc(client):
+    publication = PublicationFactory()  # rating 1
+    VoteFactory.create_batch(publication_id=publication.id, grade=True, size=3)
+    VoteFactory.create_batch(publication_id=publication.id, grade=False, size=2)
+
+    publication2 = PublicationFactory()  # rating -1
+    VoteFactory.create_batch(publication_id=publication2.id, grade=True, size=3)
+    VoteFactory.create_batch(publication_id=publication2.id, grade=False, size=4)
+
+    resp = client.get(
+        "/publications",
+        params={
+            "order_by": "rating",
+            "desc": False
+        }
+    )
+
+    true_order = [publication2, publication]
+    assert resp.status_code == status.HTTP_200_OK
+
+    publications_data = resp.json()["details"]
+    for i in range(2):
+        if publications_data[i]["id"] != true_order[i].id:
+            assert False
+
+
+def test_get_publications_ordering_by_rating_desc(client):
+    publication = PublicationFactory()  # rating 1
+    VoteFactory.create_batch(publication_id=publication.id, grade=True, size=3)
+    VoteFactory.create_batch(publication_id=publication.id, grade=False, size=2)
+
+    publication2 = PublicationFactory()  # rating -1
+    VoteFactory.create_batch(publication_id=publication2.id, grade=True, size=3)
+    VoteFactory.create_batch(publication_id=publication2.id, grade=False, size=4)
+
+    resp = client.get(
+        "/publications",
+        params={
+            "order_by": "rating",
+            "desc": True
+        }
+    )
+
+    true_order = [publication, publication2]
+    assert resp.status_code == status.HTTP_200_OK
+
+    publications_data = resp.json()["details"]
+    for i in range(2):
+        if publications_data[i]["id"] != true_order[i].id:
+            assert False
+
+
+def test_get_publications_ordering_by_creation_date(client):
+    date = datetime.datetime.now()
+    publication = PublicationFactory.create(created_at=date)
+    publication2 = PublicationFactory.create(created_at=date - datetime.timedelta(days=1))
+
+    resp = client.get(
+        "/publications",
+        params={
+            "order_by": "created_at",
+            "desc": False
+        }
+    )
+
+    true_order = [publication2, publication]
+    assert resp.status_code == status.HTTP_200_OK
+
+    publications_data = resp.json()["details"]
+    for i in range(2):
+        if publications_data[i]["id"] != true_order[i].id:
+            assert False
+
+
+def test_get_publications_ordering_by_creation_date_desc(client):
+    date = datetime.datetime.now()
+    publication = PublicationFactory.create(created_at=date)
+    publication2 = PublicationFactory.create(created_at=date - datetime.timedelta(days=1))
+
+    resp = client.get(
+        "/publications",
+        params={
+            "order_by": "created_at",
+            "desc": True
+        }
+    )
+
+    true_order = [publication, publication2]
+    assert resp.status_code == status.HTTP_200_OK
+
+    publications_data = resp.json()["details"]
+    for i in range(2):
+        if publications_data[i]["id"] != true_order[i].id:
+            assert False
